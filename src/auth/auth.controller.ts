c2dee756b29309ecd33bf2fd7e3cd73c3541ed01
kingseungil <flykim5115@gmail.com>
2023-01-18 20:37:55 +0900

    ✨ 회원가입 기능

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { UserEntity } from './entity/user.entity';
+import { AuthCredentialDto } from './dto/auth-credential';
+import { AuthService } from './auth.service';
+import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('/signup')
+    signUp(@Body() authCredentialDto: AuthCredentialDto): Promise<UserEntity> {
+        return this.authService.signUp(authCredentialDto);
+    }
+}
