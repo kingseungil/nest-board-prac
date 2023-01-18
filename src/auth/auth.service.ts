178e460ad8a893c059019624c8ece442d3187431
kingseungil <flykim5115@gmail.com>
2023-01-18 20:38:16 +0900

    ✨ 회원가입 기능

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import { AuthCredentialDto } from './dto/auth-credential';
 import { UserEntity } from './entity/user.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -9,4 +10,13 @@ export class AuthService {
         @InjectRepository(UserEntity)
         private userRepository: Repository<UserEntity>,
     ) {}
+    async signUp(authCredentialDto: AuthCredentialDto): Promise<UserEntity> {
+        const { username, password } = authCredentialDto;
+        const user = this.userRepository.create({
+            username,
+            password,
+        });
+        await this.userRepository.save(user);
+        return user;
+    }
 }
