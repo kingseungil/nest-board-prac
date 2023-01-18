fa9b6b06ced4a3af4786f3d305905e226890122e
kingseungil <flykim5115@gmail.com>
2023-01-18 21:07:31 +0900

    ✨ 유저데이터 유효성체크

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,14 @@
 import { UserEntity } from './entity/user.entity';
 import { AuthCredentialDto } from './dto/auth-credential';
 import { AuthService } from './auth.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Post('/signup')
-    signUp(@Body() authCredentialDto: AuthCredentialDto): Promise<UserEntity> {
+    signUp(@Body(ValidationPipe) authCredentialDto: AuthCredentialDto): Promise<UserEntity> {
         return this.authService.signUp(authCredentialDto);
     }
 }
--- src/auth/dto/auth-credential.ts
+++ src/auth/dto/auth-credential.ts
@@ -1,4 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialDto {
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     username: string;
+
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
+    // 영어랑 숫자만 가능하게
+    @Matches(/^[a-zA-Z0-9]*$/, {
+        message: '영어랑 숫자만 가능해요',
+    })
     password: string;
 }
